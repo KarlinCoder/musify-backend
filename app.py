a33717744e7293f845b93d0d7683ec3a89a9f78c
KarlinCoder <giancarlo.frontend@gmail.com>
2025-08-07 07:49:04 -0400

    Update

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, jsonify, send_file, send_from_directory
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from routes.download import download_song_bp
 from routes.download_album import download_album_bp
 from routes.search import search_bp
@@ -11,15 +13,27 @@ from routes.artist_discography import artist_discography_bp
 from routes.song_preview import song_preview_bp
 from routes.playlist import playlist_bp 
 from routes.playlist_tracks import playlist_tracks_bp
-from functools import wraps
-from flask import request, jsonify
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
+# Configuración del Rate Limiter
+limiter = Limiter(
+    app=app,
+    key_func=get_remote_address,  # Limita por dirección IP
+    default_limits=["200 per hour", "50 per minute"]  # Límites globales
+)
+
+# Límites específicos para rutas que consumen más recursos
+limiter.limit("120 per hour")(download_song_bp)
+limiter.limit("50 per hour")(download_album_bp)
+limiter.limit("120 per hour")(playlist_tracks_bp)
+
+# Directorio de descargas
 DOWNLOAD_DIR = './downloads'
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
+# Registro de blueprints con sus prefijos
 app.register_blueprint(download_song_bp, url_prefix='/download-song')
 app.register_blueprint(download_album_bp, url_prefix='/download-album')
 app.register_blueprint(search_bp, url_prefix="/search")
@@ -32,18 +46,17 @@ app.register_blueprint(playlist_bp, url_prefix='/playlist')
 app.register_blueprint(playlist_tracks_bp, url_prefix='/playlist-tracks')
 
 @app.route('/downloads/<path:filename>')
+@limiter.limit("50 per hour")  # Límite específico para descargas
 def download_file(filename):
     file_path = os.path.join(DOWNLOAD_DIR, filename)
     if not os.path.isfile(file_path):
         return jsonify({"error": "Archivo no encontrado"}), 404
     
-    # Forzar descarga con as_attachment=True
     return send_file(file_path, as_attachment=True)
 
 @app.route('/check')
 def check():
     return jsonify({"status": "ok"}), 200
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
